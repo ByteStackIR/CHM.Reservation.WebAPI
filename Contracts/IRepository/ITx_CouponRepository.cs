84ebaeff3c3a1ed4a07d4ad25e5def0e4ca65af6
Abbas Yavar <mr_yavar@outlook.com>
2024-08-24 22:42:53 +0330

    .

--- Contracts/IRepository/ITx_CouponRepository.cs
+++ Contracts/IRepository/ITx_CouponRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Contracts.IRepository
 {
-    public interface ITransactionRepository : IRepositoryBase<Transactions>
+    public interface ITx_CouponRepository : IRepositoryBase<Tx_Coupon>
     {
     }
 }
--- Contracts/IRepository/ITx_UserRepository.cs
+++ Contracts/IRepository/ITx_UserRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Contracts.IRepository
 {
-    public interface ITransactionRepository : IRepositoryBase<Transactions>
+    public interface ITx_UserRepository : IRepositoryBase<Tx_User>
     {
     }
 }
