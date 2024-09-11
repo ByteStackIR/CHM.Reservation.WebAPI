cd97038f0f3426da5567ce94222f6a4b3a31ae12
Abbas Yavar <mr_yavar@outlook.com>
2024-09-11 16:31:36 +0330

    RoleConfiguration

---
+++ Entities/Constant/RolesConstant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Constant
+{
+    public static class RolesConstant
+    {
+        public const string Administrator = "0650a88a-1647-4039-bbb8-af77779a1e59";
+        public const string User = "06c89d20-2688-4ba4-939c-4562838a061a";
+        public const string Manager = "42a6ba09-9eed-4d77-9f3c-fb92dfcb40e9";
+        public const string Operator = "dbd0d55e-6c75-45fa-9f33-303f06fb336d";
+    }
+}
--- Entities/ModelsConfiguration/RoleConfiguration.cs
+++ Entities/ModelsConfiguration/RoleConfiguration.cs
@@ -14,10 +14,10 @@ namespace Entities.Configurtion
         public void Configure(EntityTypeBuilder<IdentityRole> builder)
         {
             builder.HasData(
-                new IdentityRole { Name = "User", NormalizedName = "User" },
-                new IdentityRole { Name = "Administrator", NormalizedName = "ADMINISTRATOR" },
-                 new IdentityRole { Name = "Operator", NormalizedName = "OPERATOR" },
-                   new IdentityRole { Name = "Manager", NormalizedName = "MANAGER" }
+                new IdentityRole {Id = "06c89d20-2688-4ba4-939c-4562838a061a", Name = "User", NormalizedName = "User" },
+                new IdentityRole { Id = "0650a88a-1647-4039-bbb8-af77779a1e59", Name = "Administrator", NormalizedName = "ADMINISTRATOR" },
+                 new IdentityRole { Id = "dbd0d55e-6c75-45fa-9f33-303f06fb336d", Name = "Operator", NormalizedName = "OPERATOR" },
+                   new IdentityRole { Id = "42a6ba09-9eed-4d77-9f3c-fb92dfcb40e9", Name = "Manager", NormalizedName = "MANAGER" }
             );
         }
     }
