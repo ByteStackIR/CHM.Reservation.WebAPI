e89547249f32d68a886ff6b27e4473ff90f93fe5
Amin Halvaei <aminhalvaei80@gmail.com>
2024-09-25 14:56:07 +0330

    period title + Service of getting used stipend(using tx_coupon)

---
+++ Business/Services/Tx_CouponService.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Contracts.IContext;
+using Contracts.IMarker;
+using Contracts.IRepository;
+using Contracts.IService;
+using Entities.IdentityExtensions;
+using LoggerService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public class Tx_CouponService : ServiceBase, ITx_CouponService, IScopeMarker
+    {
+        public Tx_CouponService(
+            IMapper mapper,
+            ILoggerManager logger,
+            IRepositoryManager repoManger,
+            IHttpContextAccessor httpContextAccessor,
+            ISystemContext systemContext)
+            : base(repoManger, mapper, httpContextAccessor, systemContext, logger) { }
+
+        public async Task<Decimal> GetUsedAmountOfCoupon() 
+        {
+            var currentPeriodId = _systemContext.Period.Id;
+            var currentUserId = _systemContext.CurrentUser.GetUserId().Value.ToString();
+            var usedAmount = await _repositoryManager.Tx_Coupon.FindByCondition(tc => tc.PeriodId == currentPeriodId && tc.UserId == currentUserId, false)
+                                               .SumAsync(tc => tc.Amount);
+            return usedAmount;
+        }
+    }
+}
