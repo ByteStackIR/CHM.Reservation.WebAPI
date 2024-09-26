2a7e9cc5affc1f678e5bd0358d87417d5d2af223
Abbas Yavar <mr_yavar@outlook.com>
2024-09-26 18:46:03 +0330

    Parameters configuration

---
+++ Entities/ModelsConfiguration/ParametersConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Models;
+
+namespace Entities.ModelsConfiguration
+{
+
+    public class ParametersConfiguration : IEntityTypeConfiguration<Parameter>
+    {
+        public void Configure(EntityTypeBuilder<Parameter> builder)
+        {
+          
+        }
+    }
+}
