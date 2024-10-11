88f1b7dcc966f0a86fa4a41c8774b983e1d5f6f8
Amin Halvaei <aminhalvaei80@gmail.com>
2024-10-11 22:43:23 +0330

    amin migs deleted

---
+++ WebAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿
+using LoggerService;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+     public Startup(){
+     }
+    }
+}
