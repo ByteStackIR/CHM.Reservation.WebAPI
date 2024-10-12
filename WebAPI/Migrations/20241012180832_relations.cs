a56a1e4b8759b3b1401ea109ad69c1c6da27a97b
Abbas Yavar <mr_yavar@outlook.com>
2024-10-12 22:00:28 +0330

    fix bug

---
+++ WebAPI/Migrations/20241012180832_relations.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class relations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("17f944bb-9d33-4279-aed3-8f372907f27e"));
+
+            migrationBuilder.DeleteData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("18f944bb-9d33-4279-aed3-8f372907f27e"));
+
+            migrationBuilder.DeleteData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("19f944bb-9d33-4279-aed3-8f372907f27e"));
+
+            migrationBuilder.DeleteData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("20f944bb-9d33-4279-aed3-8f372907f27e"));
+
+            migrationBuilder.DeleteData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("21f944bb-9d33-4279-aed3-8f372907f27e"));
+
+            migrationBuilder.DeleteData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("22f944bb-9d33-4279-aed3-8f372907f27e"));
+
+            migrationBuilder.DeleteData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("23f944bb-9d33-4279-aed3-8f372907f27e"));
+
+            migrationBuilder.DeleteData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("24f944bb-9d33-4279-aed3-8f372907f27e"));
+
+            migrationBuilder.UpdateData(
+                table: "ObjectStates",
+                keyColumn: "Id",
+                keyValue: new Guid("99cf7da7-838f-4899-bc2f-5f9fe137bdb0"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 12, 21, 38, 31, 778, DateTimeKind.Local).AddTicks(6693));
+
+            migrationBuilder.UpdateData(
+                table: "ObjectStates",
+                keyColumn: "Id",
+                keyValue: new Guid("99cf7da7-838f-4899-bc2f-5f9fe137bdb1"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 12, 21, 38, 31, 778, DateTimeKind.Local).AddTicks(6695));
+
+            migrationBuilder.UpdateData(
+                table: "ObjectStates",
+                keyColumn: "Id",
+                keyValue: new Guid("99cf7da7-838f-4899-bc2f-5f9fe137bdb2"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 12, 21, 38, 31, 778, DateTimeKind.Local).AddTicks(6698));
+
+            migrationBuilder.UpdateData(
+                table: "ObjectStates",
+                keyColumn: "Id",
+                keyValue: new Guid("99cf7da7-838f-4899-bc2f-5f9fe137bdb3"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 12, 21, 38, 31, 778, DateTimeKind.Local).AddTicks(6701));
+
+            migrationBuilder.UpdateData(
+                table: "ObjectStates",
+                keyColumn: "Id",
+                keyValue: new Guid("99cf7da7-838f-4899-bc2f-5f9fe137bdb4"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 12, 21, 38, 31, 778, DateTimeKind.Local).AddTicks(6703));
+
+            migrationBuilder.UpdateData(
+                table: "ObjectStates",
+                keyColumn: "Id",
+                keyValue: new Guid("e790a6ce-6381-4b87-a157-5c6a73bc6470"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 12, 21, 38, 31, 778, DateTimeKind.Local).AddTicks(6681));
+
+            migrationBuilder.UpdateData(
+                table: "ObjectStates",
+                keyColumn: "Id",
+                keyValue: new Guid("e790a6ce-6381-4b87-a157-5c6a73bc6471"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 12, 21, 38, 31, 778, DateTimeKind.Local).AddTicks(6684));
+
+            migrationBuilder.UpdateData(
+                table: "ObjectStates",
+                keyColumn: "Id",
+                keyValue: new Guid("e790a6ce-6381-4b87-a157-5c6a73bc6472"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 12, 21, 38, 31, 778, DateTimeKind.Local).AddTicks(6688));
+
+            migrationBuilder.UpdateData(
+                table: "ObjectStates",
+                keyColumn: "Id",
+                keyValue: new Guid("e790a6ce-6381-4b87-a157-5c6a73bc6473"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 12, 21, 38, 31, 778, DateTimeKind.Local).AddTicks(6691));
+
+            migrationBuilder.UpdateData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-9d33-4279-aed3-8f372907f27e"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 12, 21, 38, 31, 778, DateTimeKind.Local).AddTicks(6524));
+
+            migrationBuilder.UpdateData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("10f944bb-9d33-4279-aed3-8f372907f27e"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 12, 21, 38, 31, 778, DateTimeKind.Local).AddTicks(6537));
+
+            migrationBuilder.UpdateData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-9d33-4279-aed3-8f372907f27e"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 12, 21, 38, 31, 778, DateTimeKind.Local).AddTicks(6534));
+
+            migrationBuilder.UpdateData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("11f944bb-9d33-4279-aed3-8f372907f27e"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 12, 21, 38, 31, 778, DateTimeKind.Local).AddTicks(6540));
+
+            migrationBuilder.UpdateData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("12f944bb-9d33-4279-aed3-8f372907f27e"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 12, 21, 38, 31, 778, DateTimeKind.Local).AddTicks(6541));
+
+            migrationBuilder.UpdateData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("13f944bb-9d33-4279-aed3-8f372907f27e"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 12, 21, 38, 31, 778, DateTimeKind.Local).AddTicks(6544));
+
+            migrationBuilder.UpdateData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("14f944bb-9d33-4279-aed3-8f372907f27e"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 12, 21, 38, 31, 778, DateTimeKind.Local).AddTicks(6546));
+
+            migrationBuilder.UpdateData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("15f944bb-9d33-4279-aed3-8f372907f27e"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 12, 21, 38, 31, 778, DateTimeKind.Local).AddTicks(6548));
+
+            migrationBuilder.UpdateData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("16f944bb-9d33-4279-aed3-8f372907f27e"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 12, 21, 38, 31, 778, DateTimeKind.Local).AddTicks(6550));
+
+            migrationBuilder.UpdateData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("25f944bb-9d33-4279-aed3-8f372907f27e"),
+                columns: new[] { "CreatedDate", "Title" },
+                values: new object[] { new DateTime(2024, 10, 12, 21, 38, 31, 778, DateTimeKind.Local).AddTicks(6552), "سایر" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ObjectStates",
+                keyColumn: "Id",
+                keyValue: new Guid("99cf7da7-838f-4899-bc2f-5f9fe137bdb0"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1313));
+
+            migrationBuilder.UpdateData(
+                table: "ObjectStates",
+                keyColumn: "Id",
+                keyValue: new Guid("99cf7da7-838f-4899-bc2f-5f9fe137bdb1"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1315));
+
+            migrationBuilder.UpdateData(
+                table: "ObjectStates",
+                keyColumn: "Id",
+                keyValue: new Guid("99cf7da7-838f-4899-bc2f-5f9fe137bdb2"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1318));
+
+            migrationBuilder.UpdateData(
+                table: "ObjectStates",
+                keyColumn: "Id",
+                keyValue: new Guid("99cf7da7-838f-4899-bc2f-5f9fe137bdb3"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1321));
+
+            migrationBuilder.UpdateData(
+                table: "ObjectStates",
+                keyColumn: "Id",
+                keyValue: new Guid("99cf7da7-838f-4899-bc2f-5f9fe137bdb4"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1323));
+
+            migrationBuilder.UpdateData(
+                table: "ObjectStates",
+                keyColumn: "Id",
+                keyValue: new Guid("e790a6ce-6381-4b87-a157-5c6a73bc6470"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1299));
+
+            migrationBuilder.UpdateData(
+                table: "ObjectStates",
+                keyColumn: "Id",
+                keyValue: new Guid("e790a6ce-6381-4b87-a157-5c6a73bc6471"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1303));
+
+            migrationBuilder.UpdateData(
+                table: "ObjectStates",
+                keyColumn: "Id",
+                keyValue: new Guid("e790a6ce-6381-4b87-a157-5c6a73bc6472"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1307));
+
+            migrationBuilder.UpdateData(
+                table: "ObjectStates",
+                keyColumn: "Id",
+                keyValue: new Guid("e790a6ce-6381-4b87-a157-5c6a73bc6473"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1310));
+
+            migrationBuilder.UpdateData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-9d33-4279-aed3-8f372907f27e"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1067));
+
+            migrationBuilder.UpdateData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("10f944bb-9d33-4279-aed3-8f372907f27e"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1087));
+
+            migrationBuilder.UpdateData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-9d33-4279-aed3-8f372907f27e"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1079));
+
+            migrationBuilder.UpdateData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("11f944bb-9d33-4279-aed3-8f372907f27e"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1098));
+
+            migrationBuilder.UpdateData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("12f944bb-9d33-4279-aed3-8f372907f27e"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1100));
+
+            migrationBuilder.UpdateData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("13f944bb-9d33-4279-aed3-8f372907f27e"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1102));
+
+            migrationBuilder.UpdateData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("14f944bb-9d33-4279-aed3-8f372907f27e"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1104));
+
+            migrationBuilder.UpdateData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("15f944bb-9d33-4279-aed3-8f372907f27e"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1107));
+
+            migrationBuilder.UpdateData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("16f944bb-9d33-4279-aed3-8f372907f27e"),
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1109));
+
+            migrationBuilder.UpdateData(
+                table: "Relations",
+                keyColumn: "Id",
+                keyValue: new Guid("25f944bb-9d33-4279-aed3-8f372907f27e"),
+                columns: new[] { "CreatedDate", "Title" },
+                values: new object[] { new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1125), "بدون نسبت" });
+
+            migrationBuilder.InsertData(
+                table: "Relations",
+                columns: new[] { "Id", "CreatedDate", "Maximum", "ParentId", "Title", "Type" },
+                values: new object[,]
+                {
+                    { new Guid("17f944bb-9d33-4279-aed3-8f372907f27e"), new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1110), null, new Guid("11111111-9d33-4279-aed3-8f372907f27e"), "عروس", (short)9 },
+                    { new Guid("18f944bb-9d33-4279-aed3-8f372907f27e"), new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1112), null, new Guid("11111111-9d33-4279-aed3-8f372907f27e"), "داماد", (short)10 },
+                    { new Guid("19f944bb-9d33-4279-aed3-8f372907f27e"), new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1114), null, new Guid("11111111-9d33-4279-aed3-8f372907f27e"), "خواهر", (short)8 },
+                    { new Guid("20f944bb-9d33-4279-aed3-8f372907f27e"), new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1116), null, new Guid("11111111-9d33-4279-aed3-8f372907f27e"), "برادر", (short)7 },
+                    { new Guid("21f944bb-9d33-4279-aed3-8f372907f27e"), new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1118), null, new Guid("11111111-9d33-4279-aed3-8f372907f27e"), "عمه", (short)14 },
+                    { new Guid("22f944bb-9d33-4279-aed3-8f372907f27e"), new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1119), null, new Guid("11111111-9d33-4279-aed3-8f372907f27e"), "خاله", (short)13 },
+                    { new Guid("23f944bb-9d33-4279-aed3-8f372907f27e"), new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1121), null, new Guid("11111111-9d33-4279-aed3-8f372907f27e"), "دایی", (short)12 },
+                    { new Guid("24f944bb-9d33-4279-aed3-8f372907f27e"), new DateTime(2024, 10, 10, 21, 43, 22, 889, DateTimeKind.Local).AddTicks(1123), null, new Guid("11111111-9d33-4279-aed3-8f372907f27e"), "عمو", (short)11 }
+                });
+        }
+    }
+}
